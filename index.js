a9faaa8a921cdeca49c45866997b33144cdb7569
MarcoJHB <marcothepolo@gmail.com>
2022-08-02 08:58:10 +0200

    add JS, CSS and HTML contents

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-const countriesContainer = document.querySelector(".countries");
-const search = document.querySelector(".search");
+const countriesContainer = document.querySelector('.countries');
+const dropDown = document.querySelector('.dropDown');
+const dropElem = document.querySelector('.drop');
+const region = document.querySelectorAll('.region');
+const search = document.querySelector('.search');
+const toggle = document.querySelector('.toggle');
+const moon = document.querySelector('.moon');
 
 async function getCountries() {
-  let url = "https://restcountries.com/v3.1/all";
+  let url = 'https://restcountries.com/v3.1/all';
   try {
     let res = await fetch(url);
     return await res.json();
@@ -14,21 +19,91 @@ async function getCountries() {
 async function renderCountries() {
   let countries = await getCountries();
   countries.forEach((data) => {
-    const countryCard = document.createElement("div");
-    countryCard.classList.add("country");
+    const countryCard = document.createElement('div');
+    countryCard.classList.add('country');
     countryCard.innerHTML = `
     <div class="country-img">
           <img src="${data.flags.svg}" alt="" />
         </div>
         <div class="country-info">
-          <h5>${data.name.common}</h5>
+          <h5 class="countryName">${data.name.common}</h5>
           <p><strong>Population</strong> ${data.population}</p>
-          <p><strong>Region</strong> ${data.region}</p>
+          <p class="regionName"><strong>Region</strong> ${data.region}</p>
           <p><strong>Capital</strong> ${data.capital}</p>
         </div>`;
     countriesContainer.appendChild(countryCard);
+    countryCard.addEventListener('click', () => {
+      showCountryDetail();
+    });
   });
 }
 
 renderCountries();
 
+dropDown.addEventListener('click', () => {
+  dropElem.classList.toggle('showDropDown');
+});
+
+const regionName = document.getElementsByClassName('regionName');
+const countryName = document.getElementsByClassName('countryName');
+
+region.forEach((element) => {
+  element.addEventListener('click', () => {
+    Array.from(regionName).forEach((elem) => {
+      if (elem.innerText.includes(element.innerText) || element.innerText == 'All') {
+        elem.parentElement.parentElement.style.display = 'grid';
+      } else {
+        elem.parentElement.parentElement.style.display = 'none';
+      }
+    });
+  });
+});
+
+search.addEventListener('input', () => {
+  Array.from(countryName).forEach((elem) => {
+    if (elem.innerText.toLowerCase().includes(search.value.toLowerCase())) {
+      elem.parentElement.parentElement.style.display = 'grid';
+    } else {
+      elem.parentElement.parentElement.style.display = 'none';
+    }
+  });
+});
+
+toggle.addEventListener('click', () => {
+  document.body.classList.toggle('dark');
+  moon.classList.toggle('fas');
+});
+
+const backBtn = document.querySelector('.back');
+const countryModal = document.querySelector('.countryModal');
+
+backBtn.addEventListener('click', () => {
+  countryModal.classList.toggle('show');
+});
+
+function showCountryDetail() {
+  countryModal.classList.toggle('show');
+  countryModal.innerHTML = `<button class="back">Back</button>
+  <div class="modal">
+    <div class="leftModal">
+      <img src="" alt="" />
+    </div>
+    <div class="rightModal">
+      <h1>Germany</h1>
+      <div class="country-info">
+        <div class="innerLeft inner">
+          <p><strong>Native Name:</strong> Belgie</p>
+          <p><strong>Population:</strong> Belgie</p>
+          <p><strong>Region:</strong> Belgie</p>
+          <p><strong>Sub Region:</strong> Belgie</p>
+          <p><strong>Capital:</strong> Belgie</p>
+        </div>
+        <div class="innerRight inner">
+          <p><strong>Top Level Domain:</strong> Belgie</p>
+          <p><strong>Currencies:</strong> Belgie</p>
+          <p><strong>Languages:</strong> Belgie</p>
+        </div>
+      </div>
+    </div>
+  </div>`;
+}
